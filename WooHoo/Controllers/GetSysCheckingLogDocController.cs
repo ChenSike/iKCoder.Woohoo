c44ee9dd7b42073e5515c3e7d83c9dc2f67c53be
sike.chen <sike.chen@outlook.com>
2019-09-03 01:25:54 +0800

    AAAAA

---
+++ WooHoo/Controllers/GetSysCheckingLogDocController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml;
+
+namespace WooHoo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetSysCheckingLogDocController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult action(string logname)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(logname + ".xml");
+            return Content(doc.OuterXml);
+        }
+    }
+}
