6363841753359174305a919f3379ac1a1bb75f53
sike.chen <sike.chen@outlook.com>
2019-09-06 15:59:44 +0800

    ZZZZZZZ

--- WooHoo/Controllers/SetConfAllShopingCartUpdateCountController.cs
+++ WooHoo/Controllers/SetConfAllShopingCartUpdateCountController.cs
@@ -18,19 +18,29 @@ namespace WooHoo.Controllers
         [Filter.Filter_ConnectDB]
         public ActionResult Action(int id,string guid,int count)
         {
-            Orm.Orm_conf_all_shopcart orm_Conf_All_Shopcart = new Orm.Orm_conf_all_shopcart();
-            orm_Conf_All_Shopcart.guid = guid;
-            orm_Conf_All_Shopcart.id = id;
-            orm_Conf_All_Shopcart.count = count;
-            orm_Conf_All_Shopcart.udt = DateTime.Now.ToString();
-            string query = "update conf_all_shopcart set count=@count,utd=@utd where id=@id and guid=@guid";
-            dbConnection.Execute(query, orm_Conf_All_Shopcart);
-            Conf_ResponseMessage conf_ResponseMessageObj = new Conf_ResponseMessage();
-            conf_ResponseMessageObj.code = "200";
-            conf_ResponseMessageObj.status = "ok";
-            conf_ResponseMessageObj.message = "Executed";
-            HttpContext.Response.StatusCode = 200;
-            return Json(conf_ResponseMessageObj);
+            Global.GlobalTestingLog globalTestingLog = new Global.GlobalTestingLog("ShopingCartUpdate");
+            try
+            {
+                Orm.Orm_conf_all_shopcart orm_Conf_All_Shopcart = new Orm.Orm_conf_all_shopcart();
+                orm_Conf_All_Shopcart.guid = guid;
+                orm_Conf_All_Shopcart.id = id;
+                orm_Conf_All_Shopcart.count = count;
+                orm_Conf_All_Shopcart.udt = DateTime.Now.ToString();
+                string query = "update conf_all_shopcart set count=@count,utd=@utd where id=@id and guid=@guid";
+                dbConnection.Execute(query, orm_Conf_All_Shopcart);
+                Conf_ResponseMessage conf_ResponseMessageObj = new Conf_ResponseMessage();
+                conf_ResponseMessageObj.code = "200";
+                conf_ResponseMessageObj.status = "ok";
+                conf_ResponseMessageObj.message = "Executed";
+                HttpContext.Response.StatusCode = 200;
+                return Json(conf_ResponseMessageObj);
+            }
+            catch(Exception err)
+            {
+                globalTestingLog.AddRecord("stace" , err.StackTrace);
+                globalTestingLog.AddRecord("msg" , err.Message);
+                return Content("");
+            }
         }
     }
 }
