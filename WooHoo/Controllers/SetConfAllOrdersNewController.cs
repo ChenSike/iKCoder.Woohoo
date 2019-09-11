88c088bcecec7ae1545e2d86e0f54f3e2e79a31d
sike.chen <sike.chen@outlook.com>
2019-09-11 16:07:33 +0800

    XXXXXXXXXXXXXXXX

--- WooHoo/Controllers/SetConfAllOrdersNewController.cs
+++ WooHoo/Controllers/SetConfAllOrdersNewController.cs
@@ -26,14 +26,41 @@ namespace WooHoo.Controllers
             get;
         }
 
-        public List<int> shopCartItemidLst
+        public List<JC_ConfAllOrders_Item> items
+        {
+            set;
+            get;
+        }    
+
+    }
+
+    public class JC_ConfAllOrders_Item
+    {
+        public int proid
         {
             set;
             get;
         }
 
-    }
+        public int shopcartid
+        {
+            set;
+            get;
+        }
 
+        public double price
+        {
+            set;
+            get;
+        }
+
+        public int count
+        {
+            set;
+            get;
+        }
+    }
+ 
     [Route("api/[controller]")]
     [ApiController]
     public class SetConfAllOrdersNewController : WHControllerBase
@@ -55,32 +82,21 @@ namespace WooHoo.Controllers
             {
                 JC_ConfAllOrders jC_ConfAllOrders = JsonConvert.DeserializeObject(data) as JC_ConfAllOrders;
                 string query = "";
-                double totalprice = 0.0;
-                foreach (int shopingcartitem_id in jC_ConfAllOrders.shopCartItemidLst)
+                double totalprice = 0.0;                
+                foreach (JC_ConfAllOrders_Item shopingcartitem in jC_ConfAllOrders.items)
                 {
-                    query = "select * from conf_all_shopcart where id = " + shopingcartitem_id;
-                    Orm.Orm_conf_all_shopcart orm_Conf_All_Shopcart_Item = dbConnection.Query<Orm.Orm_conf_all_shopcart>(query).FirstOrDefault();
-                    if (orm_Conf_All_Shopcart_Item != null)
+                    totalprice = totalprice + shopingcartitem.price;
+                    if(shopingcartitem.shopcartid>0)
                     {
-                        query = "select * from conf_all_proitems_store where proid = " + orm_Conf_All_Shopcart_Item.proid;
-                        Orm.Orm_conf_all_proitems_store orm_Conf_All_Proitems_Store = dbConnection.Query<Orm.Orm_conf_all_proitems_store>(query).FirstOrDefault();
-                        int stock = orm_Conf_All_Proitems_Store.stock;
-                        query = "select * from conf_all_shopcart where id=" + shopingcartitem_id;
-                        Orm.Orm_conf_all_shopcart orm_Conf_All_Shopcart = dbConnection.Query<Orm.Orm_conf_all_shopcart>(query).FirstOrDefault();
-                        if (orm_Conf_All_Shopcart.count <= stock)
-                        {
-                            query = "select * from conf_all_proitems_price where proid=" + orm_Conf_All_Shopcart_Item.proid;
-                            Orm.Orm_conf_all_proitems_price orm_Conf_All_Proitems_Price = dbConnection.Query<Orm.Orm_conf_all_proitems_price>(query).FirstOrDefault();
-                            double price = orm_Conf_All_Proitems_Price.discount > 0 ? orm_Conf_All_Proitems_Price.basic * (orm_Conf_All_Proitems_Price.discount / 100.0) : orm_Conf_All_Proitems_Price.basic;
-                            totalprice = totalprice + price;
-                        }
+                        query = "delete * from conf_all_shopcart where id=" + shopingcartitem.shopcartid;
+                        dbConnection.Execute(query);
                     }
                 }                
                 string orderid = Guid.NewGuid().ToString();
                 string cdt = DateTime.Now.ToString();
                 string returned = "0";
                 query = "insert into conf_all_orders(orderid,payed,cdt,returned,addressid,guid,totalprice) values('" + orderid + "','0','" + cdt + "','0','" + jC_ConfAllOrders.addressid + "','" + jC_ConfAllOrders.guid + "'," + totalprice + ")";
-                dbConnection.Execute(query);                
+                dbConnection.Execute(query);    
                 Conf_ResponseMessage conf_ResponseMessageObj = new Conf_ResponseMessage();
                 conf_ResponseMessageObj.code = "200";
                 conf_ResponseMessageObj.status = "OK";
