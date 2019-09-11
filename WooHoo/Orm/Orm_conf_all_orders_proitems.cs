ecda2648208de066526db246e8f571d6c055b28e
sike.chen <sike.chen@outlook.com>
2019-09-11 14:10:19 +0800

    jjjjjjjjjj

---
+++ WooHoo/Orm/Orm_conf_all_orders_proitems.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WooHoo.Orm
+{
+
+    public class Orm_conf_all_orders_proitems
+    {
+        public int id
+        {
+            set;
+            get;
+        }
+
+        public string proid
+        {
+            set;
+            get;
+        }
+
+        public string orderid
+        {
+            set;
+            get;
+        }
+
+    }
+}
