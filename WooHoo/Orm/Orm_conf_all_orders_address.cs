05172b6366c32778112ff298d380c716cb9cfeac
sike.chen <sike.chen@outlook.com>
2019-10-07 21:46:40 +0800

    XXX

---
+++ WooHoo/Orm/Orm_conf_all_orders_address.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WooHoo.Orm
+{
+    public class Orm_conf_all_orders_address
+    {
+        public int id
+        {
+            set;
+            get;
+        }
+
+        public string orderid
+        {
+            set;
+            get;
+        }
+
+        public string guid
+        {
+            set;
+            get;
+        }
+
+        public string name
+        {
+            set;
+            get;
+        }
+
+        public string country
+        {
+            set;
+            get;
+        }
+
+        public string state
+        {
+            set;
+            get;
+        }
+
+        public string city
+        {
+            set;
+            get;
+        }
+
+        public string district
+        {
+            set;
+            get;
+        }
+
+        public string address
+        {
+            set;
+            get;
+        }
+
+        public string phone
+        {
+            set;
+            get;
+        }
+                
+
+    }
+}
